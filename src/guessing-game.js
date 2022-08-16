e8e28c2284054f8c72d766ede2fad4416a7ef5fd
terrign <vajipi@gmail.com>
2022-08-16 22:34:49 +0300

    done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0
+        this.max = 0
+        this.try = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.try = Math.round((this.max + this.min) / 2)
+        return this.try
     }
 
     lower() {
-
+        this.max = this.try
     }
 
     greater() {
-
+        this.min = this.try
     }
 }
+
+const test = new GuessingGame()
+
 
 module.exports = GuessingGame;
